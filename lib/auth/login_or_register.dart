afaa2714657d18dd7b47af63a06d31396bc46916
Youngjae <2015fromjeki@gmail.com>
2025-03-19 20:46:04 +1100

    feat(page): add new loginPage,registerPage,loginorRegisterPage

---
+++ lib/auth/login_or_register.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:food/pages/login_page.dart';
+import 'package:food/pages/register_page.dart';
+
+class LoginOrRegister extends StatefulWidget {
+  const LoginOrRegister({super.key});
+
+  @override
+  State<LoginOrRegister> createState() => _LoginOrRegisterState();
+}
+
+class _LoginOrRegisterState extends State<LoginOrRegister> {
+  bool isLoginPage = true;
+  // Toggling Function
+  togglePage() {
+    setState(() {
+      isLoginPage = !isLoginPage;
+    });
+  }
+
+  // build
+  @override
+  Widget build(BuildContext context) {
+    if (isLoginPage) {
+      return LoginPage();
+    } else {
+      return RegisterPage();
+    }
+  }
+}
